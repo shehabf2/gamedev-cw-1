4d9d0966becd9537935fb565c6038993dd01a79a
shehabf2 <fnshehab2@gmail.com>
2020-12-20 11:20:55 -0500

    exercise1 completed

---
+++ Exercise1/Exercise1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Exercise1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string characterName = "Ahmed";
+            string superPower1 = "steel armor";
+            string superPower2 = "night vision";
+            int age = 15;
+            double speed = 1.65;
+
+            Console.WriteLine("I am {0} and my age is {1}. My superpowers are {2} and {3}. My speed is {4}", characterName, age, superPower1, superPower2, speed);
+        }
+    }
+}
