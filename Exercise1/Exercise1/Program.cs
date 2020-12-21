f7a840ab45029e62f1cf4d47189d4a84f82718cf
shehabf2 <fnshehab2@gmail.com>
2020-12-21 09:36:48 -0500

    exercise 2 day 2

--- Exercise1/Exercise1/Program.cs
+++ Exercise1/Exercise1/Program.cs
@@ -13,6 +13,19 @@ namespace Exercise1
             double speed = 1.65;
 
             Console.WriteLine("I am {0} and my age is {1}. My superpowers are {2} and {3}. My speed is {4}", characterName, age, superPower1, superPower2, speed);
+
+            string heroName = "Wilson";
+            string heroSuperPower1 = "programming";
+            string heroSuperpower2 = "Lighting";
+            double heroHeight = 1.65;
+            int heroAge = 23;
+            int ageDifference;
+
+            ageDifference = heroAge - age;
+            Console.WriteLine("The age Difference between {0} and {1} is {2}", heroName, characterName, ageDifference);
+
+            bool older = heroAge > age;
+            Console.WriteLine("Is {0} older than {1}? {2}", heroName, characterName, older);
         }
     }
 }
